dfd66a89b250185ff424593e99e21e2d1cccfb13
kadegnon <kodjo.adegnon@student.unamur.be>
2019-03-07 08:49:12 +0100

    Extends from JPARepository instead of CRUDRepository

--- backend/src/main/java/be/unamur/info/mdl/dal/repository/UserRepository.java
+++ backend/src/main/java/be/unamur/info/mdl/dal/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package be.unamur.info.mdl.dal.repository;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import be.unamur.info.mdl.dal.entity.User;
@@ -8,6 +9,6 @@ import be.unamur.info.mdl.dal.entity.User;
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
