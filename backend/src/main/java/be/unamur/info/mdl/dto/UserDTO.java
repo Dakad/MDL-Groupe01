b98b6a2d28a8358151d6936e6a893eade5fd3ac7
Bakary Potter <3106338+Dakad@users.noreply.github.com>
2019-03-13 00:06:11 +0100

    Config lombok annotations to not use get set prefix

--- backend/src/main/java/be/unamur/info/mdl/dto/UserDTO.java
+++ backend/src/main/java/be/unamur/info/mdl/dto/UserDTO.java
@@ -6,33 +6,37 @@ import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
-@Getter @Setter @NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(fluent = true, chain = true)
 public class UserDTO implements Serializable {
 
-    @NotBlank()
-    private String username;
+  @NotBlank()
+  private String username;
 
-    @NotBlank()
-    private String password;
+  @NotBlank()
+  private String password;
 
-    @NotBlank()
-    @Email()
-    private String email;
-
-
-    private String lastname;
+  @NotBlank()
+  @Email()
+  private String email;
 
 
-    private String firstname;
+  private String lastname;
 
 
-    public UserDTO(String username,String password,String name,String firstname,String email){
-        this.username = username;
-        this.password = password;
-        this.lastname = name;
-        this.firstname = firstname;
-        this.email = email;
-    }
+  private String firstname;
+
+
+  public UserDTO(String username, String password, String name, String firstname, String email) {
+    this.username = username;
+    this.password = password;
+    this.lastname = name;
+    this.firstname = firstname;
+    this.email = email;
+  }
 
 }
