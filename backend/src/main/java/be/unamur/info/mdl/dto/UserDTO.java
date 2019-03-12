61d12b23c7d373f62d031aa2926e0e4beea77ad1
kadegnon <kodjo.adegnon@student.unamur.be>
2019-03-12 20:00:18 +0100

    Add more contraints on UserDTO

--- backend/src/main/java/be/unamur/info/mdl/dto/UserDTO.java
+++ backend/src/main/java/be/unamur/info/mdl/dto/UserDTO.java
@@ -1,13 +1,28 @@
 package be.unamur.info.mdl.dto;
 
+import java.io.Serializable;
+import javax.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.Setter;
 
-@Getter @Setter
-public class UserDTO {
+@Getter @Setter @NoArgsConstructor
+public class UserDTO implements Serializable {
+
+    @NotBlank()
     private String username;
+
+    @NotBlank()
     private String password;
-    private String name;
-    private String firstname;
+
+    @NotBlank()
     private String email;
+
+
+    private String lastname;
+
+
+    private String firstname;
+
 }
