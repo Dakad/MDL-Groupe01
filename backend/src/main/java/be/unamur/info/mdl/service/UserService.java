e0c406a0a284052917c28aeda070d20c5554db46
bmullers <bastien.mullers@student.unamur.be>
2019-03-12 13:54:16 +0100

    Added interface methods for login

--- backend/src/main/java/be/unamur/info/mdl/service/UserService.java
+++ backend/src/main/java/be/unamur/info/mdl/service/UserService.java
@@ -7,5 +7,18 @@ public interface UserService {
 
     public boolean addUser(User user);
 
+    /**
+     *
+     * @return true if the login is successful and false if not
+     */
+    public boolean login(String username, String password);
+
+    /**
+     * A method used within the login method
+     * @param user the User object matching the username
+     * @param password
+     * @return true if the given password matches the user object's
+     */
+    public Boolean checkPassword(User user, String password);
 
 }
