7eca405d1b7e659f45c5e0e89bfab86be9b7a5b0
Bakary Potter <3106338+Dakad@users.noreply.github.com>
2019-03-13 10:43:11 +0100

    Alter UserServiceImpl class visibility for testing

--- backend/src/main/java/be/unamur/info/mdl/service/impl/UserServiceImpl.java
+++ backend/src/main/java/be/unamur/info/mdl/service/impl/UserServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Service("userService")
 @Transactional
+public
 class UserServiceImpl implements UserService {
 
   private UserRepository userRepository;
