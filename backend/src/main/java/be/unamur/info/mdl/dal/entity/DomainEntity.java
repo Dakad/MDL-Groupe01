f7e45852dda8746ea8d70e953817bd3e4a6506ea
WilliamBabe <william.turatsinze@student.unamur.be>
2019-03-14 13:53:40 +0100

    Add annotation

--- backend/src/main/java/be/unamur/info/mdl/dal/entity/DomainEntity.java
+++ backend/src/main/java/be/unamur/info/mdl/dal/entity/DomainEntity.java
@@ -1,10 +1,11 @@
 package be.unamur.info.mdl.dal.entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+@Entity
 public class DomainEntity {
   @Id
   @GeneratedValue(strategy= GenerationType.AUTO)
