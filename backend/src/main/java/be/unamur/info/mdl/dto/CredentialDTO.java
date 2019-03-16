f1a232f1cca7035cfa79b2a1f8be474894ec9cd9
Bakary Potter <3106338+Dakad@users.noreply.github.com>
2019-03-16 20:43:40 +0100

    Add Custom message on DTO Validation

--- backend/src/main/java/be/unamur/info/mdl/dto/CredentialDTO.java
+++ backend/src/main/java/be/unamur/info/mdl/dto/CredentialDTO.java
@@ -3,6 +3,7 @@ package be.unamur.info.mdl.dto;
 import java.io.Serializable;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +13,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CredentialDTO implements Serializable {
 
-  @NotBlank(message = "The username cannot be empty")
+  @NotBlank(message = "The username cannot be empty or blank")
   private String username;
 
-  @NotBlank(message = "The password cannot be empty")
+
+  @NotBlank(message = "The password cannot be empty or blank")
+  @Pattern(
+      regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[#&+-_@=£$!%*\\^\\?]?).{8,30}$",
+      message = "The password must be at least 8 characters long, containing 1 uppercase and number"
+  )
   private String password;
 }
