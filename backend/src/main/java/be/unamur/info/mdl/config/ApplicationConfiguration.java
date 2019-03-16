75d839e78a3ea910e2af17784fa656411b98e324
kadegnon <kodjo.adegnon@student.unamur.be>
2019-03-16 21:50:48 +0100

    Fix Test error : Failed to load ApplicationContext

--- backend/src/main/java/be/unamur/info/mdl/config/ApplicationConfiguration.java
+++ backend/src/main/java/be/unamur/info/mdl/config/ApplicationConfiguration.java
@@ -1,15 +1,55 @@
 package be.unamur.info.mdl.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
+//@PropertySource("application.yaml")
 public class ApplicationConfiguration {
+
+/*  @Autowired
+  private Environment env;*/
+
 
   @Bean
   public PasswordEncoder passwordEncoder(){
     return new BCryptPasswordEncoder();
   }
+
+  /*
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+
+  @Bean
+  public LocalValidatorFactoryBean validator(MessageSource messageSource){
+    LocalValidatorFactoryBean validatorFactoryBean = new LocalValidatorFactoryBean();
+    validatorFactoryBean.setValidationMessageSource(messageSource());
+    return validatorFactoryBean;
+  }
+
+  */
+
+
+/*
+  @Bean
+  public LocalValidatorFactoryBean validator(MessageSource messageSource){
+    LocalValidatorFactoryBean validatorFactoryBean = new LocalValidatorFactoryBean();
+    validatorFactoryBean.setValidationMessageSource(messageSource);
+    return validatorFactoryBean;
+
+  }
+  */
 }
