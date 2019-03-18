bf05e4ccd0f9599ffa023838b7df16f5af9adaa8
Guillaume Maitre <guillaume.maitre@student.unamur.be>
2019-03-18 11:25:55 +0100

    visual upgrade

--- frontend/src/components/AccueilVue.vue
+++ frontend/src/components/AccueilVue.vue
@@ -1,11 +1,6 @@
 <template>
 
   <section class="accueilapp">
-
-    <head>
-      <link rel="stylesheet" href="@/font-awesome-4.7.0/css/font-awesome.min.css">
-    </head>
-
     <header class="header">
       <!-- TODO mettre la barre supérieure avec login,... mais sans la barre de recherche -->
     </header>
@@ -14,45 +9,114 @@
     <div class="central">
       <div class="tittle">
         <h1> <img src="../assets/logochicken.jpg" style="width:100px;height:100px;">
-          Non Assigned Name</h1>
+          No Assigned Name</h1>
       </div>
+
       <div class="searchbar">
-        <!-- place the searched words in var searchxords and the action to
+        <!-- place the searched words in var searchwords and the action to
         script the search is searchIt -->
         <input class="searchText" v-model="searchwords" placeholder="search a state of the art">
-        <button v-on:click="searchIt"  class="fa fa-home">search</button>
+        <div class="switchbutton"><button v-on:click="searchIt"  class="fa fa-search">search</button></div>
+
       </div>
+
       <div class="filter">
 
       </div>
-      <div class="recommended">
 
-      </div>
     </div>
 
+    <div class="recommended">
+
+      <p>recommended</p>
+
+      <div class="recoNews">
+        <button v-on:click="switcher">recommander</button>
+      </div>
+
+    </div>
   </section>
 
 </template>
 
-<script>export default { };
+
+
+
+
+
+
+<script>
+  import 'swiper/dist/css/swiper.css';
+  import { swiper, swiperSlide } from 'vue-awesome-swiper';
+
+  export default { };
 </script>
 
+
+
+
+
+
 <style>
+  @import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css");
   img{
     float: left;
   }
 
   .central {
-    margin: 0;
     position: absolute;
-    top: 15%;
+    top: 20%;
     left: 50%;
     -ms-transform: translate(-50%, -50%);
     transform: translate(-50%, -50%);
+    width: 75%;
+    height: 30%;
     overflow: auto;
-    text-align: center;
   }
   h1 {
     padding: 50px;
   }
+
+  p {
+    margin-left: 15px;
+    width: 20%;
+  }
+
+  .recommended {
+    margin: 0;
+    position: absolute;
+    top: 70%;
+    left: 50%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    width: 75%;
+    height: 40%;
+    border: 3px solid gray;
+    overflow: auto;
+  }
+
+  .recoNews {
+    float: right;
+    margin-right: 50px;
+    position: absolute;
+    top: 5%;
+    left: 90%;
+  }
+
+  .searchText {
+    width: 90%;
+    height: 7%;
+    position: absolute;
+    left: 1%;
+  }
+
+  .switchbutton{
+    width: 10%;
+    height: 15%;
+    position: absolute;
+    left: 92%;
+
+  }
+
+
 </style>
