f09bea7e297638e71b4fa6acbbb9722281f79c94
kadegnon <kodjo.adegnon@student.unamur.be>
2019-03-19 14:12:11 +0100

    Ad bean for CORS filter

--- backend/src/main/java/be/unamur/info/mdl/config/ApplicationConfiguration.java
+++ backend/src/main/java/be/unamur/info/mdl/config/ApplicationConfiguration.java
@@ -9,6 +9,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 //@PropertySource("application.yaml")
@@ -22,6 +25,24 @@ public class ApplicationConfiguration {
   public PasswordEncoder passwordEncoder(){
     return new BCryptPasswordEncoder();
   }
+
+
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedOrigin("*");
+    config.addAllowedHeader("*");
+    config.addAllowedMethod("OPTIONS");
+    config.addAllowedMethod("GET");
+    config.addAllowedMethod("POST");
+    config.addAllowedMethod("PUT");
+    config.addAllowedMethod("DELETE");
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
+
 
   /*
 	@Bean
