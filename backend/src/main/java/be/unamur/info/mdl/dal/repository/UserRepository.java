1d86d699ae739971baa4b4f67a3c281a033c78a4
WilliamBabe <william.turatsinze@student.unamur.be>
2019-03-19 16:39:39 +0100

    correction des bugs

--- backend/src/main/java/be/unamur/info/mdl/dal/repository/UserRepository.java
+++ backend/src/main/java/be/unamur/info/mdl/dal/repository/UserRepository.java
@@ -1,17 +1,15 @@
 package be.unamur.info.mdl.dal.repository;
 
 
+import be.unamur.info.mdl.dal.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-import be.unamur.info.mdl.dal.entity.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    User findByUsername(String username);
+    UserEntity findByUsername(String username);
 
-    User findByEmail(String email);
+    UserEntity findByEmail(String email);
 }
