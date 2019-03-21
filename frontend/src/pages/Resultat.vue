dd8f169d6985c745f6a6c1630b1654d2af8b1dec
Bakary Potter <3106338+Dakad@users.noreply.github.com>
2019-03-22 00:44:22 +0100

    Update router

---
+++ frontend/src/pages/Resultat.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="resultat">
+    <div>
+      <p>research page</p>
+      <p>Work in progress</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Resultat"
+};
+</script>
+
+
+<style scoped>
+</style>
