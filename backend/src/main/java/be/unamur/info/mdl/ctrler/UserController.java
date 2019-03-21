235ce7c16c4dfffe9e9ad944f05066e20fce442f
kadegnon <kodjo.adegnon@student.unamur.be>
2019-03-21 22:09:26 +0100

    Create BaseApiController for common validation method btwn Controller

--- backend/src/main/java/be/unamur/info/mdl/ctrler/UserController.java
+++ backend/src/main/java/be/unamur/info/mdl/ctrler/UserController.java
@@ -3,11 +3,15 @@ package be.unamur.info.mdl.ctrler;
 import be.unamur.info.mdl.dto.PasswordChangeDTO;
 import be.unamur.info.mdl.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path="/api/user")
-public class UserController {
+public class UserController extends APIBaseController {
     @Autowired
     private UserService userService;
 
