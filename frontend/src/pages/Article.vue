22c4f337f9793a74cd55137dddc4ff4ae3f543df
Guillaume Maitre <guillaume.maitre@student.unamur.be>
2019-03-26 16:29:47 +0100

    Début de la page d'affichage d'article

---
+++ frontend/src/pages/Article.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="article">
+    <Navbar></Navbar>
+    <Search></Search>
+    <h1> {{ articleTitle }} </h1>
+    <menuArticle></menuArticle>
+    <p> {{ abstract }} </p>
+    <infoNav></infoNav>
+  </div>
+</template>
+
+<script>
+  import InfoNav from '@/components/article/InfoNav';
+  import MenuArticle from '@/components/article/MenuArticle';
+  import Navbar from '@/components/Navbar';
+
+    export default {
+        name: "Article",
+        components: {
+          InfoNav,
+          MenuArticle,
+          Navbar
+      },
+      data() {
+        return {
+          articleTitle: 'Dummy title',
+          abstract: 'Dummy Abstract that do nothing but just show you some text because why not'
+        };
+      },
+    }
+
+</script>
+
