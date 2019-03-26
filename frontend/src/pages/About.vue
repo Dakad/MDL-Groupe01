a7e2b1e19539a9f10dccd8a3ff4a0b4fe0f812e0
Christelle Trinon <ct_be@mac.com>
2019-03-26 17:47:59 +0100

    Page About avec images

--- frontend/src/pages/About.vue
+++ frontend/src/pages/About.vue
@@ -1,7 +1,174 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <b-container fluid class="About">
+    <b-row class="title">
+      <b-col>
+        <h1>About Us</h1>
+        <p>Froggosaur est une application web qui vous permettra de rechercher et de créer des états de l'art.</p>
+      </b-col>
+    </b-row>
+    
+    <b-row class="title">
+      <b-col>
+        <h2>Our Team</h2>
+      </b-col>
+    </b-row>
+
+    <div class="card-expansion">
+      <md-card>
+        <md-card-media>
+          <img src="../assets/team/christelle.jpg" alt="People">
+        </md-card-media>
+
+        <md-card-header>
+          <div class="md-title">Christelle Trinon</div>
+          <div class="md-subhead">Product Owner</div>
+        </md-card-header>
+
+        <md-card-expand>
+          <md-card-actions md-alignment="space-between">
+            <md-card-expand-trigger>
+              <md-button>Learn more</md-button>
+            </md-card-expand-trigger>
+          </md-card-actions>
+
+          <md-card-expand-content>
+            <md-card-content>
+              Description
+            </md-card-content>
+          </md-card-expand-content>
+        </md-card-expand>
+      </md-card>
+
+      <md-card>
+        <md-card-media>
+          <img src="../assets/team/guillaume.jpg" alt="People">
+        </md-card-media>
+
+        <md-card-header>
+          <div class="md-title">Guillaume Maître</div>
+          <div class="md-subhead">Scrum Master</div>
+        </md-card-header>
+
+        <md-card-expand>
+          <md-card-actions md-alignment="space-between">
+            <md-card-expand-trigger>
+              <md-button>Learn more</md-button>
+            </md-card-expand-trigger>
+          </md-card-actions>
+
+          <md-card-expand-content>
+            <md-card-content>
+              Description
+            </md-card-content>
+          </md-card-expand-content>
+        </md-card-expand>
+      </md-card>
+
+      <md-card>
+        <md-card-media>
+          <img src="../assets/team/bastien.jpg" alt="People">
+        </md-card-media>
+
+        <md-card-header>
+          <div class="md-title">Bastien Müllers</div>
+          <div class="md-subhead">Infrastructure Manager</div>
+        </md-card-header>
+
+        <md-card-expand>
+          <md-card-actions md-alignment="space-between">
+            <md-card-expand-trigger>
+              <md-button>Learn more</md-button>
+            </md-card-expand-trigger>
+          </md-card-actions>
+
+          <md-card-expand-content>
+            <md-card-content>
+              Description
+            </md-card-content>
+          </md-card-expand-content>
+        </md-card-expand>
+      </md-card>
+
+      <br>
+      <md-card>
+        <md-card-media>
+          <img src="../assets/team/david.jpg" alt="People">
+        </md-card-media>
+
+        <md-card-header>
+          <div class="md-title">David Adegnon</div>
+          <div class="md-subhead">Team Member</div>
+        </md-card-header>
+
+        <md-card-expand>
+          <md-card-actions md-alignment="space-between">
+            <md-card-expand-trigger>
+              <md-button>Learn more</md-button>
+            </md-card-expand-trigger>
+          </md-card-actions>
+
+          <md-card-expand-content>
+            <md-card-content>
+              Description
+            </md-card-content>
+          </md-card-expand-content>
+        </md-card-expand>
+      </md-card>
+
+      <md-card>
+        <md-card-media>
+          <img src="../assets/team/william.jpg" alt="People">
+        </md-card-media>
+
+        <md-card-header>
+          <div class="md-title">William Turatsinze</div>
+          <div class="md-subhead">Team Member</div>
+        </md-card-header>
+
+        <md-card-expand>
+          <md-card-actions md-alignment="space-between">
+            <md-card-expand-trigger>
+              <md-button>Learn more</md-button>
+            </md-card-expand-trigger>
+          </md-card-actions>
+
+          <md-card-expand-content>
+            <md-card-content>
+              Description
+            </md-card-content>
+          </md-card-expand-content>
+        </md-card-expand>
+      </md-card>
+
+      <md-card>
+        <md-card-media>
+          <img src="../assets/team/ludovic.jpg" alt="People">
+        </md-card-media>
+
+        <md-card-header>
+          <div class="md-title">Ludovic Wasterlain</div>
+          <div class="md-subhead">Team Member</div>
+        </md-card-header>
+
+        <md-card-expand>
+          <md-card-actions md-alignment="space-between">
+            <md-card-expand-trigger>
+              <md-button>Learn more</md-button>
+            </md-card-expand-trigger>
+          </md-card-actions>
+
+          <md-card-expand-content>
+            <md-card-content>
+              Description
+            </md-card-content>
+          </md-card-expand-content>
+        </md-card-expand>
+      </md-card>
+    </div>   
+  </b-container>
+
+
+
 </template>
 
 <script>
@@ -12,4 +179,17 @@ export default {
 
 
 <style scoped>
+  .title {
+    text-align: center;
+  }
+  .card-expansion {
+    height: 480px;
+    text-align: center;
+  }
+  .md-card {
+    width: 320px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+  }
 </style>
