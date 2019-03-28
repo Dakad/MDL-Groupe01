a572fb096daa9c7ce331a3019c2e013dd7c9dbfc
Guillaume Maitre <guillaume.maitre@student.unamur.be>
2019-03-28 11:17:32 +0100

    Not all css but better vusual

--- frontend/src/pages/Article.vue
+++ frontend/src/pages/Article.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="article">
     <Search></Search>
-    <h1> {{ articleTitle }} </h1>
-    <menuArticle></menuArticle>
-    <p> {{ abstract }} </p>
-    <infoNav></infoNav>
+    <div class="leftContainer">
+      <h1> {{ articleTitle }} </h1>
+      <div class="menuBo"><menuArticle></menuArticle></div>
+      <div class="abstrct"> <p> {{ abstract }} </p> </div>
+    </div>
+    <div class="rightContainer">
+      <infoNav></infoNav>
+    </div>
+
   </div>
 </template>
 
@@ -28,4 +33,46 @@
     }
 
 </script>
+
+<style>
+  h1{
+    position: absolute;
+    left: 10%;
+    width: 70%;
+    top : 10%;
+  }
+
+  .menuBo{
+    padding-top: 15%;
+    padding-left: 10%;
+    width: 25%;
+    float: right;
+  }
+
+  .abstrct{
+    float: bottom;
+    padding-top: 10%;
+    padding-left: 15%;
+    width: 80%;
+  }
+
+  infoNav{
+    padding-top: 10%;
+    padding: 10%;
+  }
+
+  .leftContainer{
+    position: relative;
+    float: left;
+    width: 60%;
+    height: 80%;
+  }
+
+  .rightContainer{
+    padding-right: 5px;
+    padding-top: 5%;
+    float: right;
+
+  }
+</style>
 
