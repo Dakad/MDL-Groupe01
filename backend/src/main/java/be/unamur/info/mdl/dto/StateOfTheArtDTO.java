b685852a495e527ab20b96b97b9265832115af43
Bakary Potter <3106338+Dakad@users.noreply.github.com>
2019-04-02 14:31:28 +0200

    Add tagList to ArticleDTO

--- backend/src/main/java/be/unamur/info/mdl/dto/StateOfTheArtDTO.java
+++ backend/src/main/java/be/unamur/info/mdl/dto/StateOfTheArtDTO.java
@@ -1,22 +1,27 @@
 package be.unamur.info.mdl.dto;
 
+import com.sun.tracing.dtrace.ArgsAttributes;
+import java.util.Set;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.time.LocalDate;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StateOfTheArtDTO {
   private Long id;
+
   private String name;
+
   private String subject;
+
   private LocalDate date;
+
   private UserDTO user;
 
-  public StateOfTheArtDTO(Long id, String name, String subject, LocalDate date, UserDTO user){
-    this.id = id;
-    this.name = name;
-    this.subject = subject;
-    this.date = date;
-    this.user = user;
-  }
+  private Set<TagDTO> tags;
+
 }
