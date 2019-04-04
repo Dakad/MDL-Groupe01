e39c0bfaaea98b2c65cce2b16fcf0c88c4451536
WilliamBabe <william.turatsinze@student.unamur.be>
2019-04-04 12:57:03 +0200

    Add validation on ArticleDTO

--- backend/src/main/java/be/unamur/info/mdl/dto/ArticleDTO.java
+++ backend/src/main/java/be/unamur/info/mdl/dto/ArticleDTO.java
@@ -1,26 +1,34 @@
 package be.unamur.info.mdl.dto;
 
+import java.time.LocalDate;
 import java.util.Set;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
-import java.time.LocalDate;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ArticleDTO {
-  private Long id;
 
+  private Long id;
+  
+  @NotBlank(message = "The title is required")
   private String title;
 
+  @PastOrPresent(message = "publicationDate cannot be in the futur")
   private LocalDate publicationDate;
 
+  @PositiveOrZero(message = "The price cannont be in the negative")
   private float price;
 
+  @PositiveOrZero(message = "The number of Page cannot be negative")
   private int nbrePage;
 
+  @PositiveOrZero(message = "The number of citation cannot be negative")
   private int nbreCitation;
 
   private UserDTO user;
