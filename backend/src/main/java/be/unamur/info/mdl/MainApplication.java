5dedecaa03eb0a45be261641f69faa0e51766d9a
Bakary Potter <3106338+Dakad@users.noreply.github.com>
2019-04-11 22:47:32 +0200

    Last touch to DbDataLoader before PR :-)

--- backend/src/main/java/be/unamur/info/mdl/MainApplication.java
+++ backend/src/main/java/be/unamur/info/mdl/MainApplication.java
@@ -1,9 +1,9 @@
 package be.unamur.info.mdl;
 
-import be.unamur.info.mdl.config.DBFillerRunner;
+import be.unamur.info.mdl.config.DbDataLoaderRunner;
+import be.unamur.info.mdl.dal.repository.TagRepository;
 import be.unamur.info.mdl.service.ArticleService;
 import be.unamur.info.mdl.service.UserService;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,9 +24,11 @@ public class MainApplication extends SpringBootServletInitializer {
   }
 
 
+/*
   @Bean
-  public CommandLineRunner runDBFiller(UserService userService, ArticleService articleService) {
-    return new DBFillerRunner(userService, articleService);
+  public CommandLineRunner runDBFiller(UserService userService, ArticleService articleService, TagRepository tagRepository) {
+    return new DbDataLoaderRunner(userService, articleService, tagRepository);
   }
+*/
 
 }
