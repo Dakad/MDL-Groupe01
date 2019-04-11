623d6bdaab64ed2c72673d672e11544b65bd6c61
kadegnon <kodjo.adegnon@student.unamur.be>
2019-04-11 22:33:41 +0200

    Add more findBy (name, slug) in TagRepository

--- backend/src/main/java/be/unamur/info/mdl/dal/repository/TagRepository.java
+++ backend/src/main/java/be/unamur/info/mdl/dal/repository/TagRepository.java
@@ -1,8 +1,12 @@
 package be.unamur.info.mdl.dal.repository;
 
 import be.unamur.info.mdl.dal.entity.TagEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TagRepository extends JpaRepository<TagEntity, String> {
 
+  Optional<TagEntity> findByName(String name);
+
+  Optional<TagEntity> findBySlug(String slug);
 }
