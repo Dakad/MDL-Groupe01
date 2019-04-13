a6e09107b74455f53409281f4842077a0c359716
WilliamBabe <william.turatsinze@student.unamur.be>
2019-04-13 16:15:40 +0200

    change

--- backend/src/main/java/be/unamur/info/mdl/service/StateOfTheArtService.java
+++ backend/src/main/java/be/unamur/info/mdl/service/StateOfTheArtService.java
@@ -4,4 +4,8 @@ public interface StateOfTheArtService {
 
   //methode getStateofTheArt renvoie un state of the artDTO ->go dans repository faire findbyod, lransformer en dto
 
+  //public StateOfTheArtDTO getStateOfTheArt (){
+
+  //}
+
 }
