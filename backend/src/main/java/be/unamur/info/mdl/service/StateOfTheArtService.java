58d657e01b60c89eddfe48f1764b9c8069b6db87
WilliamBabe <william.turatsinze@student.unamur.be>
2019-04-14 17:51:04 +0200

    ajout stateoftheartservice method

--- backend/src/main/java/be/unamur/info/mdl/service/StateOfTheArtService.java
+++ backend/src/main/java/be/unamur/info/mdl/service/StateOfTheArtService.java
@@ -1,11 +1,16 @@
 package be.unamur.info.mdl.service;
 
+import be.unamur.info.mdl.dto.ArticleDTO;
+import be.unamur.info.mdl.dto.StateOfTheArtDTO;
+
 public interface StateOfTheArtService {
 
   //methode getStateofTheArt renvoie un state of the artDTO ->go dans repository faire findbyod, lransformer en dto
 
-  //public StateOfTheArtDTO getStateOfTheArt (){
+  public StateOfTheArtDTO getStateOfTheArt (ArticleDTO articldto);
 
-  //}
+
+
+  public boolean SetStateofTheArt ();
 
 }
