29292cad7d6f060260fdc3744ce8039b624e1ff8
kadegnon <kodjo.adegnon@student.unamur.be>
2019-04-14 21:21:57 +0200

    Better style for Search component

--- frontend/src/components/navbar/Search.vue
+++ frontend/src/components/navbar/Search.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="search">
     <div class="searchText">
-      <md-field>
+      <md-field :md-inline="isInModeNavbar">
         <label>Type your search here</label>
         <md-input v-model="searchInput" @keyup.enter="searchIt"></md-input>
-        <span class="md-helper-text">Think well about your keywords and don't forget our filter</span>
+        <span v-if="!isInModeNavbar" class="md-helper-text">{{helperText}}</span>
         <md-button class="md-icon-button" @click="searchIt()">
           <md-icon>search</md-icon>
         </md-button>
       </md-field>
     </div>
-    <div class="searchbutton">
+    <div v-if="!isInModeNavbar" class="searchbutton">
       <md-button class="md-raised" v-on:click="searchIt()">search</md-button>
     </div>
   </div>
@@ -18,22 +18,34 @@
 
 
 <style lang="scss" scoped>
-.signin-dialog {
-  width: 55%;
-}
-// .md-dialog {
-//   widows: 100%;
-// }
 </style>
 
 
 <script>
+export const MODE_NAVBAR = "in_navbar";
+
 export default {
   name: "Search",
+  props: ["mode"],
   data: function() {
     return {
+      helperText: "Think well about your search and don't forget our filter",
       searchInput: null
     };
+  },
+  mounted() {},
+
+  computed: {
+    isInModeNavbar() {
+      return this.mode == MODE_NAVBAR;
+    },
+    placeholder() {
+      if (this.isInModeNavbar) {
+        return this.helperText.split(" and ")[0];
+      } else {
+        return null;
+      }
+    }
   },
   methods: {
     searchIt() {
