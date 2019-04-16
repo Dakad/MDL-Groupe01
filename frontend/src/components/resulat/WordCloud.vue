965724dcdb2ea5dc7c7a0db62e3414f37b25f0a5
Ludovic Wasterlain <w.ludovic@outlook.com>
2019-04-16 20:06:55 +0200

    Update WordCloud.vue

--- frontend/src/components/resulat/WordCloud.vue
+++ frontend/src/components/resulat/WordCloud.vue
@@ -3,6 +3,8 @@
     <div v-for="article in articles" v-bind:key="article">
       {{article.tags}}
     </div>
+    {{ articles[1].tags.length}}
+
   </div>
 </template>
 
@@ -14,9 +16,22 @@
     data() {
       return {
         json,
-        articles:json.results.articles,
+        articles:json.results.articles
       };
     },
+    computed:{
+      text: function(){
+        var text = String;
+        for (let i = 0; i< this.articles.length; i++) {
+          for(let j =0;j<this.articles[i].tags.length; j++){
+            text+= articles[i].tags[j] + " "
+            
+          }
+          
+        }
+        return text
+      }
+    }
 
 
   }
