b6c1c75154e158af495b9b364e6e882f97d1e210
Ludovic Wasterlain <w.ludovic@outlook.com>
2019-04-17 11:11:44 +0200

    On s'y rapproche

--- frontend/src/components/resulat/WordCloud.vue
+++ frontend/src/components/resulat/WordCloud.vue
@@ -3,7 +3,8 @@
     <div v-for="article in articles" v-bind:key="article">
       {{article.tags}}
     </div>
-    {{ articles[1].tags.length}}
+
+    {{text}}
 
   </div>
 </template>
@@ -16,25 +17,31 @@
     data() {
       return {
         json,
-        articles:json.results.articles
+        articles:json.results.articles,
       };
     },
     computed:{
       text: function(){
-        var text = String;
+        var words = [{text: '', value: 0}];
         for (let i = 0; i< this.articles.length; i++) {
           for(let j =0;j<this.articles[i].tags.length; j++){
-            text+= articles[i].tags[j] + " "
-            
+            for(let k=0;k<words.length;k++){
+              if(this.articles[i].tags[j]==words[k].text){
+                words[k].value +=1
+            }else{
+              console.log(this.articles[i].tags[j])
+              words.push({text: this.articles[i].tags[j], value: 1})
+            }
+              
           }
-          
+
         }
-        return text
+        
       }
+      return words
     }
 
 
   }
-
-
+}
 </script>
