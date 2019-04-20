4c56c24751ea4ee83b7287be5092d8bc83afee68
kadegnon <kodjo.adegnon@student.unamur.be>
2019-04-20 06:16:44 +0200

    Adapt Wordcloud component to nw props

--- frontend/src/components/resulat/WordCloud.vue
+++ frontend/src/components/resulat/WordCloud.vue
@@ -1,46 +1,58 @@
 <template>
   <div class="wordcloud">
-     <cloud :data="text" :fontSizeMapper="fontSizeMapper" />
-
-
+    <cloud :data="list" :fontSizeMapper="fontSizeMapper"/>
   </div>
 </template>
 
 <script>
-  import json from '@/assets/dummy-Results.json';
-  import Cloud from 'vue-d3-cloud';
+import json from "@/assets/dummy-Results.json";
+import Cloud from "vue-d3-cloud";
 
-  export default {
-    name: "Graphics",
-    data() {
-      return {
-        json,
-        articles:json.results.articles,
-        newtest: "coucou",
-        fontSizeMapper: word => Math.log2(word.value) * 4,
-      };
-    },
-    computed:{
-      text: function(){
-        var wordList =[]
-        var wordsUsed=[]
-        for (let i = 0; i < this.articles.length; i++){
-          for (let j = 0; j < this.articles[i].tags.length; j++) {
-            if(!wordsUsed.includes(this.articles[i].tags[j])){
-                 wordList.push({text: this.articles[i].tags[j], value: Math.floor(Math.random() * 150000) + 5000})
-                 wordsUsed.push(this.articles[i].tags[j])
-            }
-          }
-          
-        }
-        return wordList
-      }
-
-
-
+export default {
+  name: "WordCloud",
+  props: {
+    tags: {
+      type: Array,
+      required: true,
+      default: []
+    }
   },
   components: {
-        Cloud,
-    },
-}
+    Cloud
+  },
+  data() {
+    return {
+      articles: json.results.articles
+    };
+  },
+  computed: {
+    list: function() {
+      return this.tags.map(t => ({
+        text: t,
+        value: Math.floor(Math.random() * 150000) + 5000
+      }));
+    }
+    // text: function() {
+    //   var wordList = [];
+    //   var wordsUsed = [];
+    //   for (let i = 0; i < this.articles.length; i++) {
+    //     for (let j = 0; j < this.articles[i].tags.length; j++) {
+    //       if (!wordsUsed.includes(this.articles[i].tags[j])) {
+    //         wordList.push({
+    //           text: this.articles[i].tags[j],
+    //           value: Math.floor(Math.random() * 150000) + 5000
+    //         });
+    //         wordsUsed.push(this.articles[i].tags[j]);
+    //       }
+    //     }
+    //   }
+    //   return wordList;
+    // }
+  },
+  methods: {
+    fontSizeMapper(word) {
+      return word => Math.log2(word.value) * 4;
+    }
+  }
+};
 </script>
