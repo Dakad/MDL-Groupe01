b21edb468f46599eecfef57ed92cacf80fda7d0f
Guillaume Maitre <guillaume.maitre@student.unamur.be>
2019-04-22 11:33:55 +0200

    new profile version + bookmark & sota

--- frontend/src/pages/Profil.vue
+++ frontend/src/pages/Profil.vue
@@ -10,13 +10,35 @@
 
       </md-tab>
       <md-tab id="Sota" md-label="State Of The Art">
-        <Sota/>
+        <!-- TODO avoir la fonction envoyer les SOTA et adapter le code à la list reçues  -->
+        <h5>Owned SotA</h5>
+        <sota-list v-show="!loading" :list="sotaList.owned"></sota-list>
+        <md-empty-state
+          v-if="!sotaList.owned || sotaList.owned == 0"
+          md-icon="view_module"
+          md-label="No owned state of the art found"
+          md-description="Create State of the art to see them here."
+        ></md-empty-state>
+        <h5>Followed SotA</h5>
+        <sota-list v-show="!loading" :list="sotaList.followed"></sota-list>
+        <md-empty-state
+          v-if="!sotaList.followed || sotaList.followed == 0"
+          md-icon="view_module"
+          md-label="No owned state of the art found"
+          md-description="Create State of the art to see them here."
+        ></md-empty-state>
       </md-tab>
       <md-tab id="Social" md-label="Social">
 
       </md-tab>
       <md-tab id="Bookmarks" md-label="Bookmarks">
-
+        <sota-list v-show="!loading" :list="bookmarkList"></sota-list>
+        <md-empty-state
+          v-if="!bookmarkList || bookmarkList == 0"
+          md-icon="view_module"
+          md-label="No bookmark found"
+          md-description="Search for article to bookmark them."
+        ></md-empty-state>
       </md-tab>
     </md-tabs>
     </div>
@@ -25,25 +47,41 @@
 
 <script>
   import InfoBase from "@/components/profil/Info-base";
-  import Sota from "../components/profil/Sota";
-  import {getProfileBase} from "../services/api-user";
+  import {getProfileBase, getBookmark} from "../services/api-user";
+
 
   export default {
       name: "profil",
-      components: {Sota, InfoBase},
+      components: { InfoBase},
       data() {
         return{
           profil: null,
+          bookmarkList: null,
+          sotaList: null
         }
       },
       created() {
         this.fetchProfile()
+        this.fetchBookmark()
+        this.fetchSota()
       },
       methods: {
         fetchProfile(){
           getProfileBase(username).then(function(data){
             this.profil = data;
             console.log(profil);
+          })
+        },
+        fetchBookmark(){
+          getBookmark(username).then(function(data){
+            this.bookmarkList = data;
+            console.log(bookmarkList);
+          })
+        },
+        fetchSota(){
+          getBookmark(username).then(function(data){
+            this.sotaList = data;
+            console.log(sotaList);
           })
         }
       }
