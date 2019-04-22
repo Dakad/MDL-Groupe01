93f12d55bd8266e991938a6de70a6038c7d2fba0
Guillaume Maitre <guillaume.maitre@student.unamur.be>
2019-04-22 13:57:13 +0200

    correct tipo plus début info profile

---
+++ frontend/src/components/profil/MyProfile.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+      <div class="left">
+
+      </div>
+      <div class="center">
+
+      </div>
+      <div class="right">
+
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MyProfile"
+    }
+</script>
+
+<style scoped>
+  .container{
+    height: 100%;
+    width: 100%;
+  }
+  .left{
+    float: left;
+    padding-right: 5%;
+    padding-top: 5%;
+    padding-bottom: 5%;
+    height: 90%;
+    width: 30%;
+  }
+
+  .center{
+    padding-top: 5%;
+    padding-bottom: 5%;
+    height: 90%;
+    width: 30%;
+  }
+
+  .right{
+    float: right;
+    padding-left: 5%;
+    padding-top: 5%;
+    padding-bottom: 5%;
+    height: 90%;
+    width: 30%;
+  }
+
+</style>
