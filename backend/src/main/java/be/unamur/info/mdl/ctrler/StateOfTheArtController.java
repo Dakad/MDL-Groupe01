9437660a4246fc4f6e919017bfa76dbb8184acf1
WilliamBabe <william.turatsinze@student.unamur.be>
2019-04-23 06:32:45 +0200

    recommencer

--- backend/src/main/java/be/unamur/info/mdl/ctrler/StateOfTheArtController.java
+++ backend/src/main/java/be/unamur/info/mdl/ctrler/StateOfTheArtController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api/sota")
 @Api(value = "sota_controller", description = "Operations of UserControler")
 public class StateOfTheArtController {
- // @ApiOperation(value = "Return State Of the Art", response = String.class)
-  //@RequestMapping(path = "/{id}/", method = RequestMethod.GET)
- // public ResponseEntity
+
 
 }
