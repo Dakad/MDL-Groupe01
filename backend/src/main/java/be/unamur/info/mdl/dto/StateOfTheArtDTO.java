d43cc1bfa4f459cba98f9303aee5c70c1a875f86
kadegnon <kodjo.adegnon@student.unamur.be>
2019-04-24 10:30:13 +0200

    Update Entity && DTO attributes

--- backend/src/main/java/be/unamur/info/mdl/dto/StateOfTheArtDTO.java
+++ backend/src/main/java/be/unamur/info/mdl/dto/StateOfTheArtDTO.java
@@ -1,14 +1,19 @@
 package be.unamur.info.mdl.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.annotations.ApiModel;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
+import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @ApiModel(value = "SOTA", description = "Model representing a State Of The Art")
@@ -17,14 +22,27 @@ public class StateOfTheArtDTO {
   @JsonIgnoreProperties
   private Long id;
 
+  @NotBlank(message = "The name is required")
   private String name;
 
+  @NotBlank(message = "The reference is required")
+  private String reference;
+
+  @NotBlank(message = "The subject is required")
   private String subject;
+
 
   private LocalDate date;
 
+
+  @JsonAlias("creatd_at")
+  private LocalDate createdAt;
+
+
   private UserDTO creator;
 
-  private Set<TagDTO> tags;
+
+  private List<TagDTO> tags;
+
 
 }
