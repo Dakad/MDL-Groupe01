a190b7bcb637a8450812510a5431c3c99bef7486
A. K <32249902+kadegnon@users.noreply.github.com>
2019-04-25 11:31:42 +0200

    Add route to create new SoTA POST /api/sota

--- backend/src/main/java/be/unamur/info/mdl/service/impl/StateOfTheArtServiceImpl.java
+++ backend/src/main/java/be/unamur/info/mdl/service/impl/StateOfTheArtServiceImpl.java
@@ -1,15 +1,28 @@
 package be.unamur.info.mdl.service.impl;
 
+import be.unamur.info.mdl.dal.entity.ArticleEntity;
 import be.unamur.info.mdl.dal.entity.StateOfTheArtEntity;
+import be.unamur.info.mdl.dal.entity.TagEntity;
+import be.unamur.info.mdl.dal.entity.UserEntity;
+import be.unamur.info.mdl.dal.repository.ArticleRepository;
 import be.unamur.info.mdl.dal.repository.StateOfTheArtRepository;
+import be.unamur.info.mdl.dal.repository.TagRepository;
 import be.unamur.info.mdl.dal.repository.UserRepository;
 import be.unamur.info.mdl.dto.StateOfTheArtDTO;
+import be.unamur.info.mdl.dto.UserDTO;
 import be.unamur.info.mdl.service.StateOfTheArtService;
+import be.unamur.info.mdl.service.exceptions.ArticleNotFoundException;
+import be.unamur.info.mdl.service.exceptions.SotaAlreadyExistException;
 import be.unamur.info.mdl.service.exceptions.SotatNotFoundException;
+import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.DigestUtils;
 
 @Service("stateOfTheArtService")
 @Transactional
@@ -17,9 +30,18 @@ public class StateOfTheArtServiceImpl implements StateOfTheArtService {
 
   private StateOfTheArtRepository sotaRepository;
 
+  private ArticleRepository articleRepository;
+  private final UserRepository userRepository;
+  private final TagRepository tagRepository;
+
   @Autowired
-  public StateOfTheArtServiceImpl(StateOfTheArtRepository sotaRepo, UserRepository userRepo) {
+  public StateOfTheArtServiceImpl(StateOfTheArtRepository sotaRepo, UserRepository userRepo,
+    ArticleRepository articleRepo, TagRepository tagRepo) {
     this.sotaRepository = sotaRepo;
+    this.articleRepository = articleRepo;
+    this.userRepository = userRepo;
+    this.tagRepository = tagRepo;
+
   }
 
 
@@ -36,6 +58,92 @@ public class StateOfTheArtServiceImpl implements StateOfTheArtService {
       return dbSota.get().toDTO();
     }
   }
+
+
+  @Override
+  public StateOfTheArtDTO create(@Valid StateOfTheArtDTO sotaData, UserDTO currentUser)
+    throws SotaAlreadyExistException, ArticleNotFoundException {
+    if (sotaRepository.existsByReference(sotaData.getReference())) {
+      throw new SotaAlreadyExistException(
+        "The SoTA reference is already saved : " + sotaData.getReference());
+    }
+
+    if (articleRepository.existsByTitle(sotaData.getTitle())) {
+      throw new SotaAlreadyExistException(
+        "The SoTA title is already taken : " + sotaData.getTitle());
+    }
+
+    // Create a newDTO with the data sent (title, reference, ...)
+    StateOfTheArtEntity newSota = StateOfTheArtEntity.of(sotaData);
+
+    newSota.setReference(this.generateReference(sotaData.getTitle()));
+
+    UserEntity creator = userRepository.findByUsername(currentUser.getUsername());
+    newSota.setCreator(creator);
+    newSota.setCreatedAt(LocalDate.now());
+
+    this.attachReference(newSota, sotaData.getArticleList());
+
+    this.attachKeywords(newSota, sotaData.getKeywordList());
+
+    this.sotaRepository.save(newSota);
+
+    return newSota.toDTO();
+  }
+
+  /**
+   * Generate a MD5 Hash based on the provided string
+   *
+   * @param msg The provided string
+   * @return The hashed msg
+   */
+  private String generateReference(String msg) {
+    byte[] titleBytes = msg.getBytes();
+    return DigestUtils.md5DigestAsHex(titleBytes);
+  }
+
+  /**
+   * Attach the corresponding Article (persisted) to the new SoTA
+   *
+   * @param newSota The new SoTA being created
+   * @param references The article's reference list
+   * @throws ArticleNotFoundException The article reference is not in persistence.
+   */
+  private void attachReference(StateOfTheArtEntity newSota, List<String> references)
+    throws ArticleNotFoundException {
+    List<ArticleEntity> list = new LinkedList<ArticleEntity>();
+    Optional<ArticleEntity> entity;
+
+    for (String reference : references) {
+      entity = this.articleRepository.findByReference(reference);
+      if (!entity.isPresent()) {
+        throw new ArticleNotFoundException("The referenced article was not found : " + reference);
+      } else {
+        list.add(entity.get());
+      }
+    }
+    newSota.setArticles(list);
+  }
+
+  /**
+   * Attach the corresponding Author(created or persisted) to the new SoTA
+   * @param newSota The new SoTA being created
+   * @param keywords - The keyword's name list.
+   */
+  private void attachKeywords(StateOfTheArtEntity newSota, List<String> keywords) {
+    List<TagEntity> list = new LinkedList<TagEntity>();
+    TagEntity keyword;
+
+    for (String keywordName : keywords) {
+      keyword = ServiceUtils.getOrCreateTag(keywordName, this.tagRepository);
+      keyword.getStatesOfTheArts().add(newSota);
+      list.add(keyword);
+    }
+
+    newSota.setKeywords(list);
+  }
+
+
 
 
 }
