be9dae6e8a2852b67be6e3ef47dbc65c9d2432d3
Guillaume Maitre <guillaume.maitre@student.unamur.be>
2019-04-25 10:25:53 +0200

    squelette creation de SotA

--- frontend/src/components/sotaHelper/CreateSotA.vue
+++ frontend/src/components/sotaHelper/CreateSotA.vue
@@ -1,10 +1,47 @@
 <template>
     <div class="container">
       <div class="left">
+        <h4>Name of the SotA :</h4>
+        <md-field>
+          <label>Type here!</label>
+          <md-input v-model="type"></md-input>
+          <span class="md-helper-text">Helper text</span>
+        </md-field>
 
+        <h4>Main subject :</h4>
+        <md-field>
+          <label>Type here!</label>
+          <md-input v-model="type"></md-input>
+          <span class="md-helper-text">Helper text</span>
+        </md-field>
+
+        <h4>Date of the state of the art :</h4>
+        <md-field>
+          <label>Type here!</label>
+          <md-input v-model="type"></md-input>
+          <span class="md-helper-text">Helper text</span>
+        </md-field>
+
+        <h4>Authors :</h4>
+        <md-field>
+          <label>Type here!</label>
+          <md-input v-model="type"></md-input>
+          <span class="md-helper-text">Helper text</span>
+        </md-field>
+
+        <h4>Tags :</h4>
+        <md-field>
+          <label>Tags</label>
+          <md-textarea v-model="autogrow" md-autogrow></md-textarea>
+          <span class="md-helper-text">separate each tags with a coma</span>
+        </md-field>
       </div>
       <div class="right">
-
+        <h4>Import bibtex file</h4>
+          <md-field>
+            <label>Only .bibtext accepted</label>
+            <md-file v-model="multiple" multiple accept=".bibtex" />
+          </md-field>
       </div>
       <div class="bottom">
 
@@ -14,7 +51,18 @@
 
 <script>
     export default {
-        name: "CreateSotA"
+        name: "CreateSotA",
+      data: () => ({
+        initial: 'Initial Value',
+        type: null,
+        withLabel: null,
+        inline: null,
+        number: null,
+        textarea: null,
+        autogrow: null,
+        disabled: null,
+        multiple: null
+      })
     }
 </script>
 
