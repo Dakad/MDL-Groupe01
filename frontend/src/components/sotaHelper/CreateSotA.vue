8fe5c93fdc608a7ec3e8e554773d48a2d488b4d1
Guillaume Maitre <guillaume.maitre@student.unamur.be>
2019-04-25 09:07:34 +0200

    Starting sota creation

---
+++ frontend/src/components/sotaHelper/CreateSotA.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="container">
+      <div class="left">
+
+      </div>
+      <div class="right">
+
+      </div>
+      <div class="bottom">
+
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CreateSotA"
+    }
+</script>
+
+<style scoped>
+  .container{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border: grey solid 3px;
+  }
+
+  .left{
+    position: relative;
+    float: left;
+    width: 47%;
+    height: 75%;
+    margin: 1%;
+    border: green solid 3px;
+  }
+
+  .right{
+    position: relative;
+    float: left;
+    width: 47%;
+    height: 75%;
+    margin: 1%;
+    border: red solid 3px;
+  }
+
+  .bottom{
+    position: relative;
+    width: 99%;
+    height: 9%;
+    float: bottom;
+    border: blue solid 3px;
+  }
+
+
+
+
+
+</style>
