57552c8fc99f7ac28faf271bf1c73b97fb784058
Bakary Potter <3106338+Dakad@users.noreply.github.com>
2019-04-28 04:57:42 +0200

    Begin the good design on ArticleDetails Component

--- frontend/src/pages/Article.vue
+++ frontend/src/pages/Article.vue
@@ -1,16 +1,41 @@
 <template>
   <div class="md-layout md-gutter md-alignment-center-space-around">
     <div class="md-layout-item md-size-66 md-medium-size-66 md-small-size-100">
-      <div class="article-container">
-        <h1 class="article-title md-display-2">{{ article.title }}</h1>
+      <md-card md-with-hover class="article-container">
+        <md-card-header>
+          <h1 class="article-title md-display-2">{{ article.title }}</h1>
+          <div class="article-authors md-subhead">
+            <!-- <label >By</label>: -->
+            <md-button class="md-primary" v-for="(author, i) in article.authors" :key="i">{{author}}</md-button>
+          </div>
+          <div class="article-keywords">
+            <!-- <label>Keywords</label>: -->
+            <md-chip class="md-accent" title="Category : ">{{article.category}}</md-chip>
+            <md-chip v-for="keyword in article.keywords" :key="keyword.slug">{{keyword.name}}</md-chip>
+          </div>
+        </md-card-header>
+        <md-card-media-actions>
+          <md-card-area>
+            <md-content class="article-abstract">
+              <p v-for="(paragraph, i) in abstract" :key="i" class="md-subheading">{{ paragraph }}</p>
+            </md-content>
+          </md-card-area>
 
-        <div class="article-actions md-layout md-gutter md-alignment-center-right">
-          <menu-article :article-reference="article.reference"></menu-article>
-        </div>
-        <div class="article-abstract">
-          <p v-for="(paragraph, i) in abstract" :key="i" class="md-subheading">{{ paragraph }}</p>
-        </div>
-      </div>
+          <md-card-actions>
+            <md-button class="md-icon-button" title="Add to SoTA">
+              <md-icon>playlist_add</md-icon>
+            </md-button>
+
+            <md-button class="md-icon-button" title="Bookmark it">
+              <md-icon>bookmark</md-icon>
+            </md-button>
+
+            <md-button class="md-icon-button" title="Go the article URL">
+              <md-icon>get_app</md-icon>
+            </md-button>
+          </md-card-actions>
+        </md-card-media-actions>
+      </md-card>
     </div>
 
     <div class="md-layout-item md-size-25 md-medium-size-25 md-small-size-100">
@@ -19,12 +44,6 @@
       </div>
     </div>
   </div>
-
-  <!-- <div class="container">
-
-
-
-  </div>-->
 </template>
 
 <script>
@@ -97,12 +116,19 @@ export default {
 
 <style  lang="scss" scoped>
 .article-container {
+  margin-top: 15px;
+  cursor: auto;
+
   .article-title {
     margin: 3vh 0;
   }
 
-  .article-actions {
-    // float: right;
+  .article-authors button {
+    cursor: default;
+  }
+
+  .article-keywords {
+    margin: 1vh 0;
   }
 
   .article-abstract {
