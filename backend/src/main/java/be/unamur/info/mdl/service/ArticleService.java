a118a71adabdf2af0b93bb2cf8320897c5f7fb45
Bakary Potter <3106338+Dakad@users.noreply.github.com>
2019-05-01 13:41:12 +0200

    Add route to list articles by references && categories

--- backend/src/main/java/be/unamur/info/mdl/service/ArticleService.java
+++ backend/src/main/java/be/unamur/info/mdl/service/ArticleService.java
@@ -4,7 +4,10 @@ import be.unamur.info.mdl.dto.ArticleDTO;
 import be.unamur.info.mdl.dto.UserDTO;
 import be.unamur.info.mdl.service.exceptions.ArticleAlreadyExistException;
 import be.unamur.info.mdl.service.exceptions.ArticleNotFoundException;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public interface ArticleService {
 
@@ -23,4 +26,11 @@ public interface ArticleService {
    * @return The list of referenced articles
    */
   List<ArticleDTO> listArticleByReferences(List<String> references);
+
+  /**
+   * Retrieve a list of article based their categories
+   * @param categories The provided categories
+   * @return The list of referenced articles
+   */
+  Map<String, List<ArticleDTO>> listArticleByCategories(List<String> categories);
 }
