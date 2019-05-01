b0ef8ddf5a70de4990956583faa4ec4bb12e31b1
bmullers <bastien.mullers@student.unamur.be>
2019-05-01 20:31:41 +0200

    Added bookmark-remove route and fixed bug in bookmark-add

---
+++ backend/src/main/java/be/unamur/info/mdl/service/exceptions/BookmarkNotFoundException.java
@@ -0,0 +1,7 @@
+package be.unamur.info.mdl.service.exceptions;
+
+import be.unamur.info.mdl.dal.repository.BookmarkRepository;
+
+public class BookmarkNotFoundException extends Exception {
+  public BookmarkNotFoundException(){}
+}
