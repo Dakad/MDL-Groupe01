4c3dc6f687123dd7f8d245b2a7802cfabfd31c1d
Ludovic Wasterlain <w.ludovic@outlook.com>
2019-05-01 16:05:15 +0200

    squelette ok gestion

--- frontend/src/components/sotaHelper/GestionSota.vue
+++ frontend/src/components/sotaHelper/GestionSota.vue
@@ -1,4 +1,7 @@
 <template>
+    <div>
+    <div class="left">
+        <h4>Current articles in the SotA</h4>
     <ul id="articlesShow">
         <li v-for="article in articles" :key="article">
             <md-card md-with-hover>
@@ -7,13 +10,34 @@
                 </md-card-header>
 
                 <md-card-content>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+                    <p>Author : {{article.user}}</p>
+                    <p>Published on the {{article.publicationDate}}</p>
                 </md-card-content>
 
             </md-card>
             
         </li>
     </ul>
+
+    </div>
+    <div class="right"> 
+        <div class="first">
+            <h2>Sort by :</h2>
+            <md-radio v-model="sortBy" value="name">Name</md-radio>
+            <md-radio v-model="sortBy" value="title">Title</md-radio>
+            <md-radio v-model="sortBy" value="date">Date</md-radio>
+        </div>
+        <hr>
+        <div class="second">
+            <h2>Order by :</h2>
+            <md-radio v-model="orderBy" value="asc" >Ascending</md-radio>
+            <md-radio v-model="orderBy" value="desc">Descending</md-radio>
+            <small>{{ sortBy }} + {{ orderBy }}</small>
+        </div>
+        <md-button class="md-raised md-primary">Export all refs in bibTex</md-button>
+        <md-button class="md-raised md-primary">Export state of the art for offline storage</md-button>
+    </div>
+</div>
 </template>
 
 <script>
@@ -23,7 +47,10 @@ export default {
     data(){
         return{
             json,
-            articles:json.results.articles
+            articles:json.results.articles,
+            sortBy:"name",
+            orderBy:"asc"
+
         }
     }
 }
@@ -31,4 +58,26 @@ export default {
 
 <style scoped>
 
+.left{
+    position: relative;
+    float: left;
+    width: 47%;
+    height: 75%;
+    margin: 1%;
+}
+.right{
+    position: relative;
+    float: left;
+    width: 47%;
+    height: 75%;
+    margin: 1%;
+}
+.md-radio {
+  display: flex;
+}
+.md-button{
+    position: relative;
+    display:flex;
+    margin: 1%;
+}
 </style>
