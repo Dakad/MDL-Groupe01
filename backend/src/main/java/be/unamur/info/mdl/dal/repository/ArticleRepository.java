dd3eada761f8a9b31ebc314abc8d7116b8125b09
Bakary Potter <3106338+Dakad@users.noreply.github.com>
2019-05-02 13:40:29 +0200

    Fix import error && style

--- backend/src/main/java/be/unamur/info/mdl/dal/repository/ArticleRepository.java
+++ backend/src/main/java/be/unamur/info/mdl/dal/repository/ArticleRepository.java
@@ -2,13 +2,10 @@ package be.unamur.info.mdl.dal.repository;
 
 import be.unamur.info.mdl.dal.entity.ArticleEntity;
 import be.unamur.info.mdl.dal.entity.TagEntity;
-import java.util.Collection;
+import be.unamur.info.mdl.dal.entity.UserEntity;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Stream;
-
-import be.unamur.info.mdl.dal.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
