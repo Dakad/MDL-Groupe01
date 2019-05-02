32214012df70204f5c3cacfb7fc8a828975d0d62
A. K <32249902+kadegnon@users.noreply.github.com>
2019-05-02 09:19:20 +0200

    Revert "Feature/backend frontend sota creation visu"

--- backend/src/main/java/be/unamur/info/mdl/dal/repository/ArticleRepository.java
+++ backend/src/main/java/be/unamur/info/mdl/dal/repository/ArticleRepository.java
@@ -1,17 +1,13 @@
 package be.unamur.info.mdl.dal.repository;
 
 import be.unamur.info.mdl.dal.entity.ArticleEntity;
-import be.unamur.info.mdl.dal.entity.TagEntity;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Stream;
 
 import be.unamur.info.mdl.dal.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
@@ -27,8 +23,4 @@ public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
   boolean existsByReference(String reference);
 
   Stream<ArticleEntity> findDistinctByCreator(UserEntity creator, Pageable pageable);
-
-  Stream<ArticleEntity> findDistinctFirst5ByReferenceIsIn(List<String> references, Sort sortBy);
-
-  Stream<ArticleEntity> findDistinctFirst5ByCategory(TagEntity category, Sort sortBy);
 }
--- backend/src/main/java/be/unamur/info/mdl/dal/repository/TagRepository.java
+++ backend/src/main/java/be/unamur/info/mdl/dal/repository/TagRepository.java
@@ -1,19 +1,12 @@
 package be.unamur.info.mdl.dal.repository;
 
 import be.unamur.info.mdl.dal.entity.TagEntity;
-import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface TagRepository extends JpaRepository<TagEntity, String> {
 
   Optional<TagEntity> findByName(String name);
 
   Optional<TagEntity> findBySlug(String slug);
-
-  @Query("select t from TagEntity t where t.slug in :list or t.name in :list" )
-  List<TagEntity> findByNameOrSlugIn(@Param("list") List<String> categories);
 }
