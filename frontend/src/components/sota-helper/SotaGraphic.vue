db9901d90188aa57dd64529228f9189b4368214e
Bakary Potter <3106338+Dakad@users.noreply.github.com>
2019-05-08 10:41:42 +0200

    Adapt conmponent to use api articles  - 2 (ongoing)

--- frontend/src/components/sota-helper/SotaGraphic.vue
+++ frontend/src/components/sota-helper/SotaGraphic.vue
@@ -39,12 +39,11 @@ export default {
       layoutType: "euclidean", // circular
       type: "tree", // cluster
       selectedNode: null,
-      data: null
+      list: null
     };
   },
   created() {
-    this.fetchArticlesByCategories();
-    this.data = this.articles;
+    // this.fetchArticlesByCategories();
   },
   computed: {
     getCategoryFromSelectedArticles() {
@@ -52,7 +51,31 @@ export default {
     },
 
     treeDataFromArticles() {
-      return dummy;
+      const tree = Object.assign(dataTreeBase);
+      this.getCategoryFromSelectedArticles.forEach(cat => {
+        tree["children"].push({
+          name: cat,
+          children: [
+            { name: "Articles", children: [] },
+            { name: "Sota", children: [] }
+          ]
+        });
+      });
+
+      console.dir(tree);
+      this.articles.forEach(({ title, category, reference }) => {
+        const overflow = title.length > 75;
+
+        const name = !overflow ? title : title.substr(0, 75) + "...";
+
+        const categoryChild = tree["children"].find(c => c["name"] == category);
+        categoryChild["children"][0]["children"].push({
+          name,
+          reference
+        });
+      });
+
+      return tree;
     }
   },
   methods: {
@@ -61,7 +84,7 @@ export default {
     },
     fetchArticlesByCategories: function() {
       const categories = this.getCategoryFromSelectedArticles;
-      this.data = Object.assign(dataTreeBase, {});
+      this.list = Object.assign(dataTreeBase, {});
       categories.forEach(cat => {
         const child = {
           name: cat,
@@ -70,13 +93,13 @@ export default {
             { name: "Sotas", children: [] }
           ]
         };
-        data["children"].push(child);
+        this.list["children"].push(child);
       });
       // TODO Send APi request to retrieve articles in the provided categories;
       getArticlesByCategories(categories).then(respData => {
-        this.data["children"] = Object.keys(respData).reduce((list, cat) => {
-          const article = respData[cat];
-        }, []);
+        // this.list["children"] = Object.keys(respData).reduce((list, cat) => {
+        //   const article = respData[cat];
+        // }, []);
       });
     }
   }
