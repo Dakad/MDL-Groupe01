868bdf45cb58674cff447cab8d5fe84e62e990a7
Guillaume Maitre <guillaume.maitre@student.unamur.be>
2019-05-08 16:36:12 +0200

    pageSota Upgrade

--- frontend/src/pages/SotaDetails.vue
+++ frontend/src/pages/SotaDetails.vue
@@ -1,48 +1,92 @@
 <template>
   <div class="Sota">
     <div class="leftContainer">
-      <h1>{{ articleTitle }}</h1>
+      <h3>{{ articles.title }}</h3>
+      <div class="infoBaseLeft">
+        <p>Main subject: {{articles.subject}}</p>
+        <p>Date of creation: {{articles.created_at}}</p>
+      </div>
+      <div class="infoBaseRight">
+        <p>Creator: {{articles.creator.lastname}} {{articles.creator.firstname}}</p>
+        <p>Creator e-mail: {{articles.creator.email}}</p>
+      </div>
       <div class="menuBo">
-        <SotaMenu></SotaMenu>
+        <!-- TODO gerer les boutons pour telecharger le bibtex et bookmark -->
+          <SotaMenu></SotaMenu>
       </div>
-      <div class="abstract">
-        <p>{{ abstract }}</p>
-      </div>
+    </div>
+      <md-tabs
+        md-alignment="fixed"
+        :md-active-tab="activeTab"
+        class="tabSize">
 
-      <!-- TODO rajouter le templace d'affichage d'article une fois le merge fait pour SOTA -->
-      <h5>List of article in the SOTA:</h5>
-      <ul id="articleList">
-        <li v-for="(article,index) in articles" :key="index">{{ article }}</li>
-      </ul>
-    </div>
-    <div class="rightContainer">
-      <infoNav></infoNav>
-    </div>
+        <md-tab id="articleList" md-label="List of articles" md-icon="view_module">
+          <h5>List of article in the SOTA:</h5>
+
+          <article-list
+            v-show="!loading"
+            :list="articles.articles"
+          ></article-list>
+
+          <infoNav></infoNav>
+        </md-tab>
+
+        <md-tab id="visuSota" md-label="Articles-Tree" md-icon="view_module">
+          <!-- TODO Mettre la visu de David -->
+
+        </md-tab>
+
+      </md-tabs>
+
+
   </div>
 </template>
 
 <script>
 import InfoNav from "@/components/article/InfoNav";
 import SotaMenu from "@/components/sota-details/SotaMenu";
+import ArticleList from "@/components/resultat/ArticleList";
+import { getSota }  from "@/services/api-sota";
 
 export default {
   name: "Sota",
+  props: ["reference"],
   components: {
     InfoNav,
-    SotaMenu
+    SotaMenu,
+    ArticleList
   },
   data() {
     return {
-      json,
       articleTitle: {},
       abstract: {},
-      articles: []
+      articles: {}
     };
+  },
+  watch: {
+    $route: "fetchSota"
+  },
+
+  created() {
+    // fetch the data when the view is created
+    this.fetchSota();
+  },
+
+  methods:{
+    fetchSota() {
+      return getSota(this.reference).then(
+        data => (this.articles = data)
+      );
+      console.log(this.articles);
+    }
   }
 };
 </script>
 
 <style scoped>
+  .Sota{
+    border: solid lightgrey 1px;
+  }
 h1 {
   position: absolute;
   left: 10%;
@@ -51,35 +95,38 @@ h1 {
 }
 
 .menuBo {
-  padding-top: 15%;
-  padding-left: 10%;
-  width: 25%;
-  float: right;
+  float: left;
+  margin-left: 100px;
 }
 
-.abstract {
-  float: bottom;
-  padding-top: 10%;
-  padding-left: 15%;
-  width: 80%;
-}
-
-.infoNav {
-  padding-top: 10%;
-  padding: 10%;
-}
 
 .leftContainer {
   position: relative;
   float: left;
   width: 60%;
   height: 80%;
+  margin: 15px;
 }
 
 .rightContainer {
-  padding-right: 5px;
-  padding-top: 5%;
-  float: right;
+  margin-top: 15%;
+  width: 30%;
+  height: 90%;
+  float: left;
 }
+
+  .tabSize{
+    float:left;
+  }
+
+  .infoBaseLeft{
+    float: left;
+  }
+
+  .infoBaseRight{
+    float: left;
+    margin-left: 60px;
+  }
+
 </style>
 
