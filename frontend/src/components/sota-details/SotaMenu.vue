d8fcf61aa365adfee17106ca9534bd96cdc9fd44
Guillaume Maitre <guillaume.maitre@student.unamur.be>
2019-05-09 11:17:10 +0200

    duh

--- frontend/src/components/sota-details/SotaMenu.vue
+++ frontend/src/components/sota-details/SotaMenu.vue
@@ -1,20 +1,33 @@
 <template>
   <div class="menuArticle">
-    <button class="fas fa-file-download" v-on:click="downloadRef()"></button>
-    <button class="fas fa-star" v-on:click="addToSOTA()"></button>
+    <md-card-actions>
+      <md-button
+        class="md-icon-button"
+        title="Bookmark it"
+        @click="isBookmarked = !isBookmarked"
+      >
+        <md-icon>{{isBookmarked ? "bookmark" : "bookmark_border"}}</md-icon>
+      </md-button>
+
+      <md-button class="md-icon-button" title="Download" @click="downloadRef">
+        <md-icon>arrow_downward</md-icon>
+      </md-button>
+    </md-card-actions>
   </div>
 </template>
 
 <script>
 export default {
   name: "SotaMenu",
+  data(){
+    return{
+      isBookmarked: false
+    }
+  },
   methods: {
     downloadRef() {
       // TODO add the download function
     },
-    addToSOTA() {
-      // TODO add the add to SOTA function
-    }
   }
 };
 </script>
