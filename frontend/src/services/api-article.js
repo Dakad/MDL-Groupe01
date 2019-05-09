4162cadf96da289f9a9b07ad51a17b9ec2a2367c
Guillaume Maitre <guillaume.maitre@student.unamur.be>
2019-05-09 16:52:21 +0200

    bouton bookmark dans articleDetails

--- frontend/src/services/api-article.js
+++ frontend/src/services/api-article.js
@@ -17,3 +17,15 @@ export function getArticleByReference(reference) {
 export function createArticle(article) {
   return Vue.http.post('api/article', article).then(res => res.body);
 }
+
+export function getBookmarked(reference) {
+  return Vue.http.get('api/article/' + reference + '/bookmarked').then(res => res.body)
+}
+
+export function postBookmark(reference){
+  return Vue.http.post('api/article/' + reference + '/bookmark/').then(res => res.body)
+}
+
+export function deleteBookmark(reference){
+  return Vue.http.delete('api/article/' + reference + '/bookmark').then(res => res.body)
+}
