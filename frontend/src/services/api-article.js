9cae2a70cdec27e6ba125e4b451a5a4c5c4c0bf3
kadegnon <kodjo.adegnon@student.unamur.be>
2019-05-09 18:59:31 +0200

    Add auth headers to create sota

--- frontend/src/services/api-article.js
+++ frontend/src/services/api-article.js
@@ -5,7 +5,6 @@
 import Vue from 'vue';
 
 // Mine
-import { getAuthToken } from './storage';
 import { getAuthHeaders } from './api';
 
 // -------------------------------------------------------------------
@@ -20,6 +19,5 @@ export function getArticleByReference(reference) {
 
 export function createArticle(article) {
   const headers = getAuthHeaders();
-  // TODO Add the token in the request header to create the new article
-  return Vue.http.post('api/article', article, headers).then(res => res.body);
+  return Vue.http.post('/api/article', article, headers).then(res => res.body);
 }
