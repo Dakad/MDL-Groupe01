2164e3133fae7d4f7d75be1189efb921ab2625a8
kadegnon <kodjo.adegnon@student.unamur.be>
2019-05-09 05:27:08 +0200

    Adapt html

--- frontend/src/components/sota-helper/SotaCreate.vue
+++ frontend/src/components/sota-helper/SotaCreate.vue
@@ -1,52 +1,93 @@
 <template>
-  <div class="md-layout md-gutter">
-    <div class="md-layout-item">
-      <md-field>
-        <label>Name of the SotA</label>
-        <md-input v-model="sotaName"></md-input>
-        <span class="md-helper-text">Helper text</span>
-      </md-field>
-      <md-field>
-        <label>Main subject</label>
-        <md-input v-model="domain"></md-input>
-        <span class="md-helper-text">Helper text</span>
-      </md-field>
-      <md-field>
-        <label>Authors</label>
-        <md-input v-model="author"></md-input>
-        <span class="md-helper-text">Helper text</span>
-      </md-field>
+  <div class>
+    <form class="md-layout md-gutter md-alignment-center-space-between">
+      <div class="md-layout-item md-size-100 md-layout md-gutter">
+        <div class="md-layout-item">
+          <md-field>
+            <md-icon>event</md-icon>
+            <label>
+              Name of the SotA
+              <span class="md-subheading">(REQUIRED)</span>
+            </label>
+            <md-input v-model="sota.title"></md-input>
+            <span class="md-helper-text"></span>
+          </md-field>
 
-      <md-field>
-        <label>Tags</label>
-        <md-textarea v-model="tags" md-autogrow></md-textarea>
-        <span class="md-helper-text">separate each tags with a coma</span>
-      </md-field>
-    </div>
-    <div class="md-layout-item">
-      <md-field>
-        <label>Import bibtex file</label>
-        <md-file
-          multiple
-          accept=".bib, .bibtex"
-          v-model="uploads"
-          @md-change="onFileUpload($event)"
-        />
-      </md-field>
-    </div>
-    <div class="bottom">
-      <b-button size="lg" variant="outline-info" @click="showAcceptMessage = true">UPLOAD THE SotA</b-button>
-    </div>
+          <md-field>
+            <md-icon>event</md-icon>
+            <label>Main subject</label>
+            <md-input v-model="sota.domain"></md-input>
+            <!-- <span class="md-helper-text">Helper text</span> -->
+          </md-field>
+
+          <md-field>
+            <md-icon>event</md-icon>
+            <label>Keywords</label>
+            <md-textarea v-model="sota.keywords" md-autogrow></md-textarea>
+            <span class="md-helper-text">
+              Separate each tags with a coma
+              <!-- <strong class="md-headline">,</strong> -->
+            </span>
+          </md-field>
+          <br>
+          <md-field>
+            <label>Import bibtex file</label>
+            <md-file
+              multiple
+              accept=".bib, .bibtex"
+              v-model="uploads"
+              @md-change="onFileUpload($event)"
+            />
+          </md-field>
+        </div>
+
+        <!-- Import bibtex field -->
+        <div class="md-layout-item md-size-45">
+          <h3 class="md-title">Preview of processed .bibtex</h3>
+          <pre id="bibtex-preview">{{preview.json}}</pre>
+        </div>
+      </div>
+
+      <!-- Upload btn -->
+      <div class="md-layout-item md-size-100" id="upload-btn-container">
+        <md-button
+          class="md-raised md-primary"
+          :md-ripple="false"
+          @click="showAcceptMessage = true"
+        >Upload the SoTA</md-button>
+      </div>
+    </form>
+
+    <md-dialog-confirm
+      class="sota-create-dialog"
+      :md-active.sync="showAcceptMessage"
+      md-title="Confirm this sota creation"
+      md-content="&nbsp;Do you really want to upload this SotA ?"
+      md-confirm-text="Agree"
+      md-cancel-text="No, return"
+      @md-cancel="showAcceptMessage = false"
+      @md-confirm="sendSota"
+    />
+    <md-dialog-alert md-title="SoTA created!" md-content="Your SoTA has been uploaded."/>
+
+    <md-dialog :md-active.sync="showCreatedMessage">
+      <md-dialog-title>SoTA created</md-dialog-title>
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="showCreatedMessage = false">Okay</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+
     <!--
-    <md-dialog class="login-dialog" :md-active.sync="showAcceptMessage">
+    <md-dialog-confirm :md-active.sync="showAcceptMessage">
       <md-dialog-title>
-        &nbsp;Are you sure you want to upload this SotA
         <div class="bottom-acc">
           <b-button size="lg" variant="outline-info" @click="sendSota()">Yes</b-button>
           <b-button size="lg" variant="outline-info" @click="showAcceptMessage = false">No, return</b-button>
         </div>
       </md-dialog-title>
-    </md-dialog>-->
+
+      <md-button class="md-primary md-raised" @click="active = true">Confirm</md-button>
+    </md-dialog-confirm>-->
   </div>
 </template>
 
@@ -58,14 +99,38 @@ export default {
   name: "CreateSota",
   data() {
     return {
-      sotaName: null,
-      domain: null,
-      author: null,
-      tags: null,
+      sota: {
+        title: null,
+        keywords: null,
+        domain: null
+      },
       uploads: null,
       articlesUploaded: [],
-      showAcceptMessage: false
+      preview: {
+        json: null,
+        bibtex: null
+      },
+      showAcceptMessage: false,
+      showCreatedMessage: false
     };
+  },
+
+  computed: {
+    listAuthors() {
+      return [
+        { name: "Aqua", color: "#00ffff" },
+        { name: "Olive", color: "#808000" },
+        { name: "Orange", color: "#ffa500" },
+        { name: "Orange Red", color: "#ff4500" },
+        { name: "Pale Golden Rod", color: "#eee8aa" },
+        { name: "Pale Green", color: "#98fb98" },
+        { name: "Teal", color: "#008080" },
+        { name: "Turquoise", color: "#40e0d0" },
+        { name: "Violet", color: "#ee82ee" },
+        { name: "White", color: "#ffffff" },
+        { name: "Yellow", color: "#ffff00" }
+      ];
+    }
   },
 
   methods: {
@@ -74,8 +139,9 @@ export default {
 
       reader.onload = e => {
         // this.bibtex.push(e.target.result);
+        this.preview["bibtex"] = e.target.result;
         const bib2Json = bibParser(e.target.result);
-        window.alert(bib2Json);
+        this.preview["json"] = bib2Json;
         this.articlesUploaded = this.articlesUploaded.concat(bib2Json);
       };
       reader.readAsText(event.item(0));
@@ -87,18 +153,18 @@ export default {
 
       // TODO Create an api-article.js to create and get an article
 
-      const articeRefs = []
+      const articeRefs = [];
       const createArticleRequests = this.articlesUploaded.map(article => {
-        articleRefs.push(article['reference']);
-        return createArticle(article)
+        articleRefs.push(article["reference"]);
+        return createArticle(article);
       });
 
       // Send all request to create an article, fail
-      Promise.race(createArticleRequests).then(values => {
-        console.log(values);
-
-      }).catch(console.error);
-
+      Promise.race(createArticleRequests)
+        .then(values => {
+          console.log(values);
+        })
+        .catch(console.error);
 
       // TODO Send an API call to create the new SoTA
     }
@@ -107,33 +173,21 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  position: absolute;
-  width: 100%;
-  height: 100%;
+#upload-btn-container {
+  text-align: center;
+  margin: 40px;
 }
 
-.left {
-  position: relative;
-  float: left;
-  width: 47%;
-  height: 75%;
-  margin: 1%;
+#bibtex-preview {
+  padding: 10px;
+  max-height: 450px;
+  white-space: pre-wrap;
+  font-size: 13px;
+  border: 3px solid gray;
+  overflow-y: hidden;
 }
 
-.right {
-  position: relative;
-  float: left;
-  width: 47%;
-  height: 75%;
-  margin: 1%;
-}
-
-.bottom {
-  position: relative;
-  width: 99%;
-  height: 9%;
-  float: bottom;
-  padding-left: 40%;
+#bibtex-preview:hover {
+  overflow-y: visible;
 }
 </style>
