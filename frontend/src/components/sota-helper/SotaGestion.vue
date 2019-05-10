628313d51db0e18fd812bc41f3f5271938f293ca
Guillaume Maitre <guillaume.maitre@student.unamur.be>
2019-05-10 17:20:05 +0200

    remove the useless buttons

--- frontend/src/components/sota-helper/SotaGestion.vue
+++ frontend/src/components/sota-helper/SotaGestion.vue
@@ -33,7 +33,6 @@
         <small>{{ sortBy }} + {{ orderBy }}</small>
       </div>
       <md-button class="md-raised md-primary">Export all refs in bibTex</md-button>
-      <md-button class="md-raised md-primary">Export SotA for offline storage</md-button>
     </div>
   </div>
 </template>
