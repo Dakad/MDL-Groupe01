179001759dca3ef5d30e1aa7358fa7dc593c75f6
Bakary Potter <3106338+Dakad@users.noreply.github.com>
2019-05-11 22:59:16 +0200

    Fix compilation warning

--- backend/src/main/java/be/unamur/info/mdl/dto/UserDTO.java
+++ backend/src/main/java/be/unamur/info/mdl/dto/UserDTO.java
@@ -1,15 +1,12 @@
 package be.unamur.info.mdl.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 @Data
 @NoArgsConstructor
@@ -25,14 +22,11 @@ public class UserDTO extends CredentialDTO {
 
   private String firstname;
 
-  @Builder.Default
   @JsonProperty("avatar")
   private String profilePicUrl = "https://i.imgur.com/0MC7ZG4.jpg";
 
-  @Builder.Default
   private String domain = "Unknown";
 
-  @Builder.Default
   private String organisation = "Not defined";
 
 }
