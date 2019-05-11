6948560946b84b9c5a2817a51e303921c7146431
Bakary Potter <3106338+Dakad@users.noreply.github.com>
2019-05-11 18:36:35 +0200

    Add function to parse to Json and bibtex

--- frontend/src/services/bibtex-parse.js
+++ frontend/src/services/bibtex-parse.js
@@ -14,7 +14,7 @@ import bibtexParser from 'bibtex-parse-js';
  *
  * @param {*} json
  */
-export function format(json) {
+function format(json) {
   const abstract = json.entryTags['abstract'];
   if (abstract == undefined || abstract.trim().length()) {
     json.entryTags['abstract'] = json.entryTags['title'];
@@ -34,7 +34,7 @@ export function format(json) {
  *
  * @param {*} json
  */
-export function transform(json) {
+function transform(json) {
   return Object.assign({}, json.entryTags, {
     reference: json.citationKey,
     authors: json.entryTags.author.split(', ').map(a => a.trim()),
@@ -52,4 +52,34 @@ export function parse(bibtex) {
     .toJSON(bibtex)
     .map(format)
     .map(transform);
+}
+
+export function toBibtex(json) {
+  let bibtex = '';
+  if (Array.isArray(json)) {
+    bibtex = json
+      .map(value => parseJsonToBibtex(value))
+      .reduce((bibtex, str) => (bibtex += str), '');
+  } else {
+    bibtex = parseJsonToBibtex(json);
+  }
+  return bibtex;
+}
+
+function parseJsonToBibtex(article) {
+  const fields = Object.keys(article);
+  let bibtex = `@book{${article.reference},`;
+  // Loop over the fields of the article
+  Object.keys(article).forEach((field, i, list) => {
+    // write them in the str
+    bibtex += `${field} = ${JSON.stringify(article[field])}`;
+    if (i < list.length - 1) {
+      bibtex += ',';
+    }
+  });
+
+  // Close the  bibtex
+  bibtex += '}\n\n';
+
+  return bibtex;
 }
