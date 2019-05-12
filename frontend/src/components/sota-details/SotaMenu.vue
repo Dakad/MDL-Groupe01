7daafd9a4792778810588f9bc9b3f3a0acf2d60d
kadegnon <kodjo.adegnon@student.unamur.be>
2019-05-12 08:39:32 +0200

    Functional download JSON

--- frontend/src/components/sota-details/SotaMenu.vue
+++ frontend/src/components/sota-details/SotaMenu.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="menuArticle">
-    <md-card-actions>
-      <md-button class="md-icon-button" :title="bookmarkBtnTitle" @click="bookmarkMe">
-        <md-icon>{{isBookmarked ? "bookmark" : "bookmark_border"}}</md-icon>
-      </md-button>
+    <md-button class="md-icon-button" :title="bookmarkBtnTitle" @click="bookmarkMe">
+      <md-icon>{{isBookmarked ? "bookmark" : "bookmark_border"}}</md-icon>
+    </md-button>
 
-      <md-button class="md-icon-button" title="Download" @click="downloadRef">
-        <md-icon>get_app</md-icon>
-      </md-button>
-    </md-card-actions>
+    <md-button class="md-icon-button" title="Download" @click="downloadRef">
+      <md-icon>get_app</md-icon>
+    </md-button>
   </div>
 </template>
 
