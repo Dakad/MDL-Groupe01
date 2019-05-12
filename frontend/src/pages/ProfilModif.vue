e7cb4f15a98ea3cff6cf932025e4125ce1ad847f
Guillaume Maitre <guillaume.maitre@student.unamur.be>
2019-05-12 10:41:17 +0200

    test1

--- frontend/src/pages/ProfilModif.vue
+++ frontend/src/pages/ProfilModif.vue
@@ -3,45 +3,50 @@
       <div class="left">
         <div class="md-layout">
           <md-field>
+            <md-icon class="material-icons">
+              insert_photo
+            </md-icon>
             <label>Change your avatar</label>
             <md-input v-model="avatarimg"></md-input>
           </md-field>
+          <md-autocomplete v-model="university" :md-options="universities">
+            <md-icon class="material-icons">
+              home
+            </md-icon>
+            <label>Change your university</label>
+          </md-autocomplete>
+          <md-autocomplete v-model="domain" :md-options="domains" >
+            <md-icon class="material-icons">
+              keyboard
+            </md-icon>
+            <label>Change your domain</label>
+          </md-autocomplete>
           <md-field>
-            <label>Change your University</label>
-            <md-input v-model="university"></md-input>
-          </md-field>
-          <md-field>
-            <label for="domain">Change your main research domain</label>
-            <md-select v-model="domain" name="domain" id="domain">
-              <md-option value="ComputerScience">Computer Science</md-option>
-              <md-option value="Literature">Literature</md-option>
-              <md-option value="Chemistry">Chemistry</md-option>
-              <md-option value="Physics">Physics</md-option>
-              <md-option value="Biology">Biology</md-option>
-              <md-option value="Medicine">Medicine</md-option>
-              <md-option value="Economics">Economics</md-option>
-              <md-option value="Psychology">Psychology</md-option>
-              <md-option value="Laws">Laws</md-option>
-              <md-option value="Mathematics">Mathematics</md-option>
-              <md-option value="Veterinary">Veterinary</md-option>
-              <md-option value="History">History</md-option>
-              <md-option value="Engineering">Engineering</md-option>
-              <md-option value="Language">Language</md-option>
-            </md-select>
+            <label>Change your bio</label>
+            <md-textarea v-model="bio"></md-textarea>
           </md-field>
         </div>
       </div>
       <div class="right">
         <md-field>
           <label>Change your email address</label>
-          <md-input v-model="emailAddress"></md-input>
+          <md-icon class="material-icons">
+            email
+          </md-icon>
+          <md-input v-model="emailAddress" ></md-input>
         </md-field>
         <md-field>
           <label>Change your research groups</label>
+          <md-icon class="material-icons">
+            group
+          </md-icon>
           <md-input v-model="rGroup" md-autogrow></md-input>
         </md-field>
         <md-field>
           <label>Change your interest</label>
+          <md-icon class="material-icons">
+            location_searching
+          </md-icon>
           <md-input v-model="interest" md-autogrow></md-input>
         </md-field>
       </div>
@@ -58,6 +63,12 @@
         rGroup: 'Nadi, IRIDIA, BRUH',
         interest: 'Infovis, Bruh, IT',
         avatarimg: 'link to image',
+        domains: ["ComputerScience", "Literature", "Chemistry", "Physics",
+          "Biology", "Medicine", "Economics", "Economics", "Psychology",
+          "Laws", "Mathematics", "Veterinary", "History", "Engineering",
+          "Language"],
+        universities: ["UNamur", "ULB", "Ulg", "UMons", "Kul", "Oxford", "MIT"],
+        bio: "BLELBLE",
       }),
       created() {
           //todo get the information from BD
