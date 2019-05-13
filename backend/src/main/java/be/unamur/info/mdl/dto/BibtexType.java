50d5a2e3aedff33cbd1f4ede39e806b449539e65
bmullers <bastien.mullers@student.unamur.be>
2019-05-14 01:07:30 +0200

    Added type to article entities and dto

---
+++ backend/src/main/java/be/unamur/info/mdl/dto/BibtexType.java
@@ -0,0 +1,17 @@
+package be.unamur.info.mdl.dto;
+
+public enum BibtexType {
+  book,
+  booklet,
+  conference,
+  inbook,
+  incollection,
+  inproceedings,
+  manual,
+  mastersthesis,
+  misc,
+  phdthesis,
+  proceedings,
+  techreport,
+  unpublished
+}
