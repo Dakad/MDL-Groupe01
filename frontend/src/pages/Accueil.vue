d71dfbdcddd754db0efbd4cdefce3f7c2e688e78
kadegnon <kodjo.adegnon@student.unamur.be>
2019-05-14 01:56:43 +0200

    Clean & remove commented codes

--- frontend/src/pages/Accueil.vue
+++ frontend/src/pages/Accueil.vue
@@ -3,16 +3,12 @@
     <div class="central md-layout-item md-size-70 md-layout md-alignment-top-center">
       
       <div class="head md-layout-item md-size-80 md-layout md-gutter md-alignment-top-center">
-        <img class="logoimg md-layout-item md-size-15" src="../assets/logo-app.png" style="width:90px;height:100px;">
+        <img class="logoimg md-layout-item md-size-15" src="@/assets/logo-app.png" style="width:90px;height:100px;">
         <div class="title md-layout-item md-size-40"><h1>Froggosaur</h1></div>
         <!-- ou md-size-35? -->
       </div>
 
       <div class="search md-layout-item md-size-80">
-        <h2>{{msg}}</h2>
-
-        <!-- place the searched words in var searchwords and the action to
-        script the search is searchIt-->
 
         <search id="search" @error="msg = $event"></search>
       </div>
@@ -68,26 +64,10 @@ export default {
       articles: dummyArticles,
     };
   },
-  searchIt() {}
 };
 </script>
 
 <style scoped>
-/* img {
-  float: left;
-} */
-
-/* .logoimg {
-  width: 10%;
-  height: 10%;
-  position: absolute;
-  left: 20%;
-}
-
-.title {
-  position: absolute;
-  left: 35%;
-} */
 
 .head {
   margin-top: 2%;
@@ -98,9 +78,6 @@ h1 {
   margin-top: 35px;
 }
 
-/* #search {
-  margin-top: 100px;
-} */
 
 .search {
   margin-top: 2%;
