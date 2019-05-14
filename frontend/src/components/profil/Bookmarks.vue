b009079f923e56b02bf5ddc5e2d246a73753809b
Guillaume Maitre <guillaume.maitre@student.unamur.be>
2019-05-14 22:47:11 +0200

    sotahelper-overview working
    Generate a lot of unknow error

--- frontend/src/components/profil/Bookmarks.vue
+++ frontend/src/components/profil/Bookmarks.vue
@@ -1,14 +1,21 @@
 <template>
     <div id="bookmarks">
-    <h2 v-if="articles.length!=0">Articles bookmarked</h2>
         <div class="article">
             <div v-for="item in articles" :key="item">
                 <md-card md-with-hover>
                 <md-card-header>
                 <router-link :to="{ name: 'articleDetails', params: { reference: item.reference }}">
-                    <h4>{{item.title}}</h4>
+                    <h6>{{item.title}}</h6>
                 </router-link>
                 </md-card-header>
+                  <md-card-content>
+                    <div class="left1">
+                      <p>Domain: {{ item.category }}</p>
+                    </div>
+                    <div class="right1">
+                      <p>Year of publication: {{ item.year }} </p>
+                    </div>
+                  </md-card-content>
                 </md-card>
             </div>
         </div>
@@ -60,4 +67,13 @@ export default {
 </script>
 
 <style scoped>
+
+  .left1{
+    float: left;
+    margin-right: 5%;
+  }
+  .right1{
+    float: left;
+    margin-left: 5%;
+  }
 </style>
