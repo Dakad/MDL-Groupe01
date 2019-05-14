9df4977e8148059caafe470c554ba49d9b924089
kadegnon <kodjo.adegnon@student.unamur.be>
2019-05-14 10:09:07 +0200

    Use SortOptions on SotaOverview

--- frontend/src/components/sota-helper/SotaOverview.vue
+++ frontend/src/components/sota-helper/SotaOverview.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <div class="left">
+  <div class="md-layout md-gutter">
+    <div class="md-layout-item md-size-75">
       <h4>Current articles in the SotA</h4>
-      <ul id="articlesShow">
-        <li v-for="article in articles" :key="article">
+      <!-- <ul id="articlesShow">
+        <li v-for="article in sortedArticlesList" :key="article">
           <md-card md-with-hover>
             <md-card-header>
               <div class="md-title">{{article.title}}</div>
@@ -15,61 +15,72 @@
             </md-card-content>
           </md-card>
         </li>
-      </ul>
+      </ul>-->
+
+      <article-list :list="sortedArticlesList"></article-list>
     </div>
-    <div class="right">
+    <div class="md-layout-item">
       <!-- TODO Replace by The SortOption Component used on the Result page -->
-      <div class="first">
-        <h2>Sort by :</h2>
-        <md-radio v-model="sortBy" value="name">Name</md-radio>
-        <md-radio v-model="sortBy" value="title">Title</md-radio>
-        <md-radio v-model="sortBy" value="date">Date</md-radio>
-      </div>
-      <hr>
-      <div class="second">
-        <h2>Order by :</h2>
-        <md-radio v-model="orderBy" value="asc">Ascending</md-radio>
-        <md-radio v-model="orderBy" value="desc">Descending</md-radio>
-        <small>{{ sortBy }} + {{ orderBy }}</small>
-      </div>
-      <md-button class="md-raised md-primary">Export all refs in bibTex</md-button>
+      <sort-options
+        class="sort-container"
+        :sort="sortBy"
+        :order="orderBy"
+        active="articles"
+        @change:sort="sortBy = $event"
+        @change:order="orderBy = $event"
+      >
+        <md-button class="md-raised md-primary">Export all refs in bibTex</md-button>
+      </sort-options>
     </div>
   </div>
 </template>
 
 
 <script>
+import ArticleList from "@/components/resultat/ArticleList";
+import SortOptions from "@/components/resultat/SortOptions";
+
 export default {
   name: "SotaOverview",
+  components: {
+    ArticleList,
+    SortOptions
+  },
+  props: {
+    list: Array
+  },
   data() {
     return {
-      data: {},
-      articles: {},
-      sortBy: "name",
+      // data: {},
+      articles: [],
+      sortBy: "title",
       orderBy: "asc"
     };
+  },
+  computed: {
+    sortedArticlesList() {
+      const fnSortArticles = (a1, a2) => {
+        if (!Object.keys(a1).includes(this.sortBy)) {
+          return 0;
+        }
+        if (this.orderBy.localeCompare("asc")) {
+          return a1[this.orderBy].localeCompare(a2[this.orderBy]);
+        } else {
+          return a2[this.orderBy].localeCompare(a1[this.orderBy]);
+        }
+      };
+      return this.list.sort(fnSortArticles);
+    }
   }
 };
 </script>
 
 <style scoped>
-.left {
-  position: relative;
-  float: left;
-  width: 70%;
-  height: 75%;
-  margin: 1%;
+.sort-options {
+  position: fixed;
+  z-index: 75;
 }
-.right {
-  position: relative;
-  float: left;
-  width: 20%;
-  height: 75%;
-  margin: 1%;
-}
-.md-radio {
-  display: flex;
-}
+
 .md-button {
   position: relative;
   display: flex;
