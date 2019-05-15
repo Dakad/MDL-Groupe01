b36ab783f4d4fda67ae776f8b73e8e854f867451
Bakary Potter <3106338+Dakad@users.noreply.github.com>
2019-05-15 05:39:27 +0200

    :clap: @bmullers You are now officialy a Full-Stack Developer

--- frontend/src/pages/Subscriptions.vue
+++ frontend/src/pages/Subscriptions.vue
@@ -1,50 +1,55 @@
 <template>
-    <section class="md-layout">
-        <div><h2>Articles recently posted by people you follow :</h2></div>
-        <div class="articles">
-            <article-list v-show="!loading" :list = "articles"></article-list>
-        </div>
-    </section>
+  <section class="md-layout">
+    <div>
+      <h2>Recent articles posted by your followings :</h2>
+    </div>
+    <div class="articles">
+      <article-list v-show="!loading" :list="articles"></article-list>
+    </div>
+  </section>
 </template>
 
 <script>
-import ArticleList from "@/components/resultat/ArticleList"
-import { getSubscriptions } from '../services/api-article';
+import ArticleList from "@/components/resultat/ArticleList";
+import { getSubscriptions } from "@/services/api-article";
 export default {
-    name: 'Subscriptions',
-    components: {
-        ArticleList
-    },
+  name: "Subscriptions",
+  components: {
+    ArticleList
+  },
 
-    data() {
-        return {
-            articles: {},
-            loading : false,
-        };
-    },
+  data() {
+    return {
+      articles: {},
+      loading: false
+    };
+  },
 
-    created(){
-        this.fetchSubscriptions();
-    },
+  created() {
+    this.fetchSubscriptions();
+  },
 
-    methods:{
-        fetchSubscriptions(){
-            return getSubscriptions()
-                .then(data => {this.articles = data;console.log(data);!this.loading});
-        }
-    },
-}
+  methods: {
+    fetchSubscriptions() {
+      this.loading = true;
+      return getSubscriptions().then(data => {
+        this.articles = data;
+        this.loading = false;
+      });
+    }
+  }
+};
 </script>
 
 <style scoped>
-h2{
-    margin-top : 25px;
-    margin-left : 40px;
+h2 {
+  margin-top: 25px;
+  margin-left: 40px;
 }
 
-.articles{
-    margin-top : 35px;
-    margin-left : 5%;
-    margin-right: 15%;
+.articles {
+  margin-top: 35px;
+  margin-left: 5%;
+  margin-right: 15%;
 }
 </style>
