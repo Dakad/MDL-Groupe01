9515667b9ba2277e597e0cd33a53cd2ccf69dec9
kadegnon <kodjo.adegnon@student.unamur.be>
2019-05-15 04:37:07 +0200

    Send flash message on follow/unfollow actions

--- frontend/src/components/profil/StatsLink.vue
+++ frontend/src/components/profil/StatsLink.vue
@@ -1,42 +1,59 @@
 <template>
-  <div class="cont" v-if="!(this.username==null)" >
-  <md-button class="md-raised md-accent" :md-ripple="false" v-if="isFollowed" @click="follow">
-    Stop following
-  </md-button>
-   <md-button class="md-raised md-primary" :md-ripple="false" v-if="!isFollowed" @click="follow">
-     Follow
-   </md-button>
+  <div class="cont" v-if="!(this.username==null)">
+    <md-button
+      class="md-raised md-accent"
+      :md-ripple="false"
+      v-if="isFollowed"
+      @click="follow"
+    >Stop following</md-button>
+    <md-button
+      class="md-raised md-primary"
+      :md-ripple="false"
+      v-if="!isFollowed"
+      @click="follow"
+    >Follow</md-button>
   </div>
 </template>
 
 <script>
-  import { postFollow, postUnFollow, getFollow } from "@/services/api-user";
+import { postFollow, postUnFollow, getFollow } from "@/services/api-user";
+import { EventBus, EVENT_APP_MESSAGE } from "@/services/event-bus.js";
 
 export default {
   data() {
     return {
-      isFollowed : false,
-      username: null,
+      isFollowed: false,
+      username: null
     };
   },
 
-  methods:{
-    fetchFollowed(){
+  methods: {
+    fetchFollowed() {
       getFollow(this.username).then(data => {
-        this.isFollowed = data;
+        this.isFollowed = data["done"] || false;
       });
     },
 
     follow() {
       if (!this.isFollowed) {
-        postFollow(this.username)
-          .then(x => (this.isFollowed = true))
+        postFollow(this.username).then(x => {
+          this.isFollowed = true;
+          EventBus.$emit(EVENT_APP_MESSAGE, {
+            type: "info",
+            msg: "Your are now following " + this.username
+          });
+        });
       } else if (this.isFollowed) {
-        postUnFollow(this.username)
-          .then(x => (this.isFollowed = false))
+        postUnFollow(this.username).then(x => {
+          console.log(x);
+          this.isFollowed = false;
+          EventBus.$emit(EVENT_APP_MESSAGE, {
+            type: "info",
+            msg: "Your are not following " + this.username + " anymore"
+          });
+        });
       }
-    },
-
+    }
   },
 
   created() {
@@ -48,5 +65,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
